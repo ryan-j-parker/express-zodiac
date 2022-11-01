d6cf72aa82508dda1a96b8336fdcf76cb21b7f75
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-01 15:16:04 -0700

    add horoscope test and route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,3 +24,15 @@ describe('zodiac routes', () => {
     expect(res.body).toEqual(sagittarius);
   });
 });
+
+describe('horoscope routes', () => {
+  it('/horoscopes/:sign should return a single horoscope by sign', async () => {
+    const res = await request(app).get('/horoscopes/leo');
+    const leo = {
+      name: 'leo',
+      horoscope:
+        'Try not to let your heart and mind spiral if the object of your desires seems closed off or distant this morning, dear Leo, as the Aquarius moon squares off with the nodes of fate. This cosmic climate will trigger emotional shutdowns within many, making it important that you give your sweetie some space if they need it. Be sure to focus on self-care and supporting yourself as the hours unfold, especially when Luna squares off with Uranus this afternoon. This celestial exchange will cause you to hit a wall if your balance is struggling, so be sure to give yourself a timeout.',
+    };
+    expect(res.body).toEqual(leo);
+  });
+});
