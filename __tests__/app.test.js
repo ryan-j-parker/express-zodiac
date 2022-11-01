05cd10fdf87af043dbf8c1fc843799b4bd49fbfb
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-01 13:40:30 -0700

    add tests and routes for zodiac list and id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,26 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiac } = require('../lib/zodiac-data');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+describe('zodiac routes', () => {
+  it('/zodiac should return a list of zodiac signs', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiac.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+});
+
+describe('zodiac routes', () => {
+  it('/zodiac/:id should return a single zodiac sign by ID', async () => {
+    const res = await request(app).get('/zodiac/9');
+    const sagittarius = {
+      id: '9',
+      name: 'sagittarius',
+      dates: 'Nov 22 - Dec 21',
+      symbol: 'Archer',
+    };
+    expect(res.body).toEqual(sagittarius);
   });
 });
